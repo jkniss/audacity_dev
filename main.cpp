15ea05a26f95e98a2939a754f0d7e3256252dde5
Jesse Kniss <jessekniss05@gmail.com>
2024-04-07 11:11:58 -0600

    Added ExpandNeighbors, CheckValidCell, updated State enum and Search method

--- main.cpp
+++ main.cpp
@@ -8,17 +8,23 @@
 
 using namespace std;
 
+//directional deltas
+const int delta[4][2]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+
 /* We want to have our board display various states instead of zeros and ones.
  Creating an enum allows us to build a custom class with custom outputs.
  The enum State will be used to describe the state of the baord instead of integers.
  All the functions will be refactored to accept a State object instead of an int*/
-enum class State {kEmpty, kObstacle, kClosed, kPath};
+enum class State {kEmpty, kObstacle, kClosed, kPath, kStart, kFinish};
 
 /* Function that takes an enum and returns the state as a string value
  * Enum is declared above. State {kEmpty, kObstacle}*/
 string CellString(enum State cell) {
     switch(cell) {
         case State::kObstacle: return "⛰   ";
+        case State::kStart: return "🚦   ";
+        case State::kFinish: return "🏁   ";
         default: return "0   ";
     }
 }
@@ -83,13 +89,14 @@ void PrintBoard(const vector<vector<State>>& board) {
  * if the f-value of the first argument is greater than f-value of second argument, otherwise return false*/
 
 bool Compare(vector<int> node1, vector<int> node2) {
-    int f1 = node1[2] + node2[3];
-    int f2 = node1[2] + node2[3];
+    int f1 = node1[2] + node2[3]; // f1 = g1 + h1
+    int f2 = node1[2] + node2[3]; // f2 = g2 + h2
 
     return f1 > f2;
 }
 
 /*CellSort function uses the Compare function to determine the sorting order*/
+
 void CellSort(vector<vector<int>> *v) {
     sort(v->begin(), v->end(), Compare);
 }
@@ -108,7 +115,8 @@ void AddToOpen( int x,  int y,  int g,  int h, vector<vector<int>> &open_nodes, 
     // Set the grid value for the x and y coordinates to the enum value kClosed
     grid[x][y] = State::kClosed;
 }
-/* Heuristic function, takes for ints as arguments. The ints represent two pairs
+
+/* Heuristic function, takes four ints as arguments. The ints represent two pairs
   of 2D coordinates (x1, y1, x2, y2). Function returns an int which is the
   Manhattan Distance from one coordinate to the other: |x2-x1| + |y2-y1|
   */
@@ -118,10 +126,47 @@ int Heuristic( int x1,  int y1,  int x2,  int y2) {
     return manhattan_distance;
 }
 
+bool CheckValidCell(int x, int y, vector<vector<State>> grid) {
+    // Check the (x,y) coordinate pair is on the grid
+    bool on_grid_x = (x >= 0 && x < grid.size());
+    bool on_grid_y = (y >= 0 && y < grid[0].size());
+
+    // Check that grid at (x,y) is kEmpty (default case if grid cell is not kClosed or kObstacle)
+    if (on_grid_x && on_grid_y)
+        // If both conditions are true, return true.
+        return grid[x][y] == State::kEmpty;
+    return false;
+
+}
+
+void ExpandNeighbors(const vector<int> &current_node, int goal[2], vector<vector<int>> &open_nodes, vector<vector<State>> &grid) {
+    // Get current node's data {x, y, g}
+    int x = current_node[0];
+    int y = current_node[1];
+    int g = current_node[2];
+
+    //Loop through current node's potential neighbors. Use the directional deltas to check values
+    for (int i =0; i < 4; i++) {
+        int x2 = x + delta[i][0];
+        int y2 = y + delta[i][1];
+
+        // Check that the potential neighbor's x2 and y2 values are on the grid and not closed.
+        if (CheckValidCell(x2, y2, grid)) {
+            // Increment g value and add neighbor to open list
+            int g2 = g + 1;
+            int h2 = Heuristic(x2, y2, goal[0], goal[1]);
+            AddToOpen(x2, y2, g2, h2, open_nodes, grid);
+        }
+    }
+
+}
+
 /* Function that takes a 2D State vector and two int arrays[2] as arguments.
  returns a 2D board vector*/
 
 vector<vector<State>> Search(vector<vector<State>> grid, int initial_point[2], int goal_point[2]) {
+
+
 
     // Create the vector of open nodes
     vector<vector<int>> open {};
@@ -160,10 +205,15 @@ vector<vector<State>> Search(vector<vector<State>> grid, int initial_point[2], i
         grid[cur_x][cur_y] = State::kPath;
 
         if (cur_x == goal_point[0] &&  cur_y == goal_point[1]) {
+            // Set initial coordinates in grid to kStart
+            grid[initial_point[0]][initial_point[1]] = State::kStart;
+
+            // Set goal coordinates in grid to kFinish
+            grid[goal_point[0]][goal_point[1]] = State::kFinish;
             return grid;
         }
         else {
-            cout << "Run expandNeighbors function here";
+            ExpandNeighbors(current_node, goal_point, open, grid);
         }
     }
 
@@ -171,7 +221,8 @@ vector<vector<State>> Search(vector<vector<State>> grid, int initial_point[2], i
     return vector<vector<State>> {};
 }
 
-int main() {
+
+    int main() {
 
     int init[2]{0,0};
     int goal[2]{4, 5};
